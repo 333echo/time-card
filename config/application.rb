a390fcdd2e2f6c69cd9f5299c03e38d6d48807d3
333echo <3echo.law@gmail.com>
2020-02-29 21:49:13 +0900

    initial commit

---
+++ config/application.rb
@@ -0,0 +1,23 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module TimeCard
+  class Application < Rails::Application
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
+    # config~end:不要なファイルを生成しないようにする　https://master.tech-camp.in/curriculums/1171
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+  end
+end
